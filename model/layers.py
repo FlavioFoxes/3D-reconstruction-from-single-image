2f42779244acb071cd2f1a7b656e5d1197ff604b
Flavio <flaviovolpi2000@gmail.com>
2024-04-25 01:31:19 +0200

    finished the down sampling block of the attention module. Now everything is in Network to check the shapes of the outputs

--- model/layers.py
+++ model/layers.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from basic_layers import ResidualBlock
 
 class Network(nn.Module):
     def __init__(self, input_channels):
@@ -11,13 +12,35 @@ class Network(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding='same')
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding='same')
 
+        # ATTENTION MODULE (must be put in basic_layers)
+        # Now it's here to check the shapes
+        self.res1 = ResidualBlock(input_channels = 32, output_channels = 32)
+        
+        self.max1 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.res2 = ResidualBlock(input_channels = 32, output_channels=32)
+
+        self.skip = ResidualBlock(input_channels=32, output_channels=32)
+
+        self.max2 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.res3 = ResidualBlock(input_channels = 32, output_channels=32)
 
     def forward(self, x):
         x = self.padding1(x)  # Apply padding for conv1
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
+        x = self.res1(x)
 
+        x = self.max1(x)
+        x = self.res2(x)
+
+        # Here skip has shape [1, 32, 56, 56]
+        skip = self.skip(x)
+        
+        x = self.max2(x)
+        x = self.res3(x)
+        # Here the x has shape [1, 32, 28, 28]
+        
         print("Final shape of x:    ", x.shape)
         return x
 
