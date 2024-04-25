e2bcd208cad4bcada4b0d3c688e64a17a7d463d4
FlavioFoxes <flaviovolpi2000@gmail.com>
2024-04-25 20:44:44 +0200

    attention module DONE

--- model/basic_layers.py
+++ model/basic_layers.py
@@ -48,9 +48,62 @@ class UpsamplingBlock(nn.Module):
 class AttentionModule(nn.Module):
     def __init__(self, input_channels, output_channels):
         super(AttentionModule, self).__init__()
-        self.res1 = ResidualBlock(input_channels=input_channels, output_channels=output_channels)
-        self.res2 = ResidualBlock(input_channels=output_channels, output_channels=...)
-        self.res3 = ResidualBlock(input_channels=..., output_channels=...)
+        self.res1 = ResidualBlock(input_channels = input_channels, output_channels = output_channels)
+        
+        # SOFT MASK branch
+        self.max1 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.res2 = ResidualBlock(input_channels = output_channels, output_channels=output_channels)
+
+        self.skip = ResidualBlock(input_channels = output_channels, output_channels=output_channels)
+
+        self.max2 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.res3 = ResidualBlock(input_channels = output_channels, output_channels=output_channels)
+
+        self.res4 = ResidualBlock(input_channels = output_channels, output_channels=output_channels)
+        self.up1 = nn.Upsample(size=(56, 56))
+
+        self.res5 = ResidualBlock(input_channels=output_channels, output_channels=output_channels)
+        self.up2 = nn.Upsample(size=(112,112))
+
+        self.conv = nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=1)
+
+        # TRUNK branch
+        self.res1_trunk = ResidualBlock(input_channels = output_channels, output_channels=output_channels)
+        self.res2_trunk = ResidualBlock(input_channels = output_channels, output_channels=output_channels)
+
+        self.res_final = ResidualBlock(input_channels = output_channels, output_channels=output_channels)
+
+    def forward(self, x):
+        x = self.res1(x)
+
+        # TRUNK branch
+        trunk_x = self.res1_trunk(x)
+        trunk_x = self.res2_trunk(x)
+
+        # SOFT MASK branch
+        x = self.max1(x)
+        x = self.res2(x)
+        ## Here skip has shape [1, 32, 56, 56]
+        skip = self.skip(x)
+        x = self.max2(x)
+        x = self.res3(x)
+        ## Here the x has shape [1, 32, 28, 28]
+        x = self.res4(x)
+        x = self.up1(x)
+        x = x + skip
+        x = self.res5(x)
+        x = self.up2(x)
+        x = self.conv(x)
+        x = self.conv(x)
+        x = F.sigmoid(x)
+
+        # Union of the two branches
+        x = torch.matmul(x, trunk_x)
+        x = x + trunk_x
+        x = self.res_final(x)
+
+        return x
+
 
         
         
--- model/layers.py
+++ model/layers.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-from basic_layers import ResidualBlock
+import torch.nn.functional as F
+from basic_layers import ResidualBlock, AttentionModule
 
 class Network(nn.Module):
     def __init__(self, input_channels):
@@ -10,36 +11,40 @@ class Network(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=16, kernel_size=5, stride=2)
         # conv2
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding='same')
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding='same')
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding='same')
 
         # ATTENTION MODULE (must be put in basic_layers)
         # Now it's here to check the shapes
-        self.res1 = ResidualBlock(input_channels = 32, output_channels = 32)
+        # self.res1 = ResidualBlock(input_channels = 64, output_channels = 64)
         
-        self.max1 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.res2 = ResidualBlock(input_channels = 32, output_channels=32)
+        # self.max1 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        # self.res2 = ResidualBlock(input_channels = 64, output_channels=64)
 
-        self.skip = ResidualBlock(input_channels=32, output_channels=32)
+        # self.skip = ResidualBlock(input_channels = 64, output_channels=64)
 
-        self.max2 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.res3 = ResidualBlock(input_channels = 32, output_channels=32)
+        # self.max2 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        # self.res3 = ResidualBlock(input_channels = 64, output_channels=64)
+
+        # self.res4 = ResidualBlock(input_channels= 64, output_channels=64)
+        # self.up1 = nn.Upsample(size=(56, 56))
+
+        # self.res5 = ResidualBlock(input_channels=64, output_channels=64)
+        # self.up2 = nn.Upsample(size=(112,112))
+
+        # self.conv_a1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1)
+
+        self.attention1 = AttentionModule(input_channels= 64, output_channels=64)
+
 
     def forward(self, x):
         x = self.padding1(x)  # Apply padding for conv1
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = self.res1(x)
-
-        x = self.max1(x)
-        x = self.res2(x)
-
-        # Here skip has shape [1, 32, 56, 56]
-        skip = self.skip(x)
+        # # Here x has shape [1, 64, 112, 112]
         
-        x = self.max2(x)
-        x = self.res3(x)
-        # Here the x has shape [1, 32, 28, 28]
+        x = self.attention1(x)
+        # # Here x has shape [1, 64, 112, 112]
         
         print("Final shape of x:    ", x.shape)
         return x
